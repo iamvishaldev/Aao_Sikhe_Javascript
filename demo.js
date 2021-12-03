3ef3c5dde19ef1a1d2bf586619f4cbf95f5a443c
iamvishaldev <yvishal706@gmail.com>
2021-12-03 19:49:50 +0530

    Checking sum zero - problem 1

--- demo.js
+++ demo.js
@@ -1,10 +1,18 @@
 // Checking sum zero - problem 1
-// [-5, -4,-3,-2,0,2,4,6,8] ---> input
+// remove first pair whoes sum is zero
+// [-5,-4,-3,-2,0,2,4,6,8] ---> input
+// [-4,4] --> output
 
 const getSumPair = (array) => {
     for (let number of array) {
-        console.log(number)
+        console.log("Outer Loop")
+        for (let j = 1; j < array.length; j++) {
+            console.log("Inner Loop")
+            if (number + array[j] === 0) {
+                return [number, array[j]]
+            }
+        }
     }
 }
-
-getSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8])
+const result = getSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8])
+console.log(result)
