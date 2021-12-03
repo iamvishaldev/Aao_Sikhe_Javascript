9294f35fa287c2859b60702eb5a0258ae2fadf7e
iamvishaldev <yvishal706@gmail.com>
2021-12-03 09:03:18 +0530

    first commit

--- demo.js
+++ demo.js
@@ -1,2 +1,10 @@
 // Checking sum zero - problem 1
 // [-5, -4,-3,-2,0,2,4,6,8] ---> input
+
+const getSumPair = (array) => {
+    for (let number of array) {
+        console.log(number)
+    }
+}
+
+getSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8])
